2b6a738a04f71c9b979c17b46d66aa61916b5565
Feihong Hsu <feihong.hsu@gmail.com>
2020-12-20 09:10:24 -0600

    Add script to convert dumpfile to sqlite db

--- manhua/README.md
+++ manhua/README.md
@@ -6,6 +6,12 @@ Examine requests visually
 
     mitmweb -r dumpfile
 
+Convert to sqlite database file
+
+    mitmdump -ns to_sqlite.py -r dumpfile
+
+Use [DB Browser for SQLite](https://sqlitebrowser.org/) to view database contents
+
 ## Analysis
 
 - ComicDetail
---
+++ manhua/to_sqlite.py
@@ -0,0 +1,61 @@
+"""
+Dump flows into sqlite database so that you can randomly access them.
+"""
+import os.path
+from pathlib import Path
+import sqlite3
+import typing
+
+import mitmproxy.http
+from mitmproxy import ctx
+
+filename = 'dumpfile.db'
+create_table_statement = """
+CREATE TABLE dump (
+  path text,
+  content_type text,
+  xhash text,
+  request_body text,
+  data blob
+)"""
+
+class SqliteAddon:
+    def __init__(self):
+      if os.path.exists(filename):
+        os.remove(filename)
+      self.conn = sqlite3.connect(filename)
+      self.cur = self.conn.cursor()
+      self.cur.execute(create_table_statement)
+      self.conn.commit()
+
+    def response(self, flow: mitmproxy.http.HTTPFlow):
+        """
+        The full HTTP response has been read.
+        """
+        try:
+          # ctx.log.info(flow.request.path)
+          res = flow.response
+          content_type = res.headers.get('content-type')
+          if content_type:
+            self.cur.execute(
+              'INSERT INTO dump VALUES (?, ?, ?, ?, ?)',
+              (
+                flow.request.path,
+                content_type,
+                res.headers.get('x-hash'),
+                flow.request.content,
+                res.content,
+              )
+            )
+        except Exception as e:
+          ctx.log.error(str(e))
+
+        self.conn.commit()
+
+    def done(self):
+        self.conn.close()
+        print('\nDone!\n')
+
+addons = [
+    SqliteAddon()
+]
