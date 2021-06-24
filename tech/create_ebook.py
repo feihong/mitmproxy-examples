1168c23ccbcf95defb4ff94b9ea7241382505fb2
Feihong Hsu <feihong.hsu@gmail.com>
2021-06-24 12:32:58 -0500

    Leave HTML formatting in comments

--- tech/create_ebook.py
+++ tech/create_ebook.py
@@ -2,39 +2,29 @@ import sys
 import subprocess
 from pathlib import Path
 import json
-from html.parser import HTMLParser
 
-def html2text(html):
-  text = ''
-  class MyParser(HTMLParser):
-      def handle_data(self, data):
-          nonlocal text
-          text += data
-
-  parser = MyParser()
-  parser.feed(html)
-  return text
 
 book_dir = Path('book' if len(sys.argv) <= 1 else sys.argv[1])
-
 meta = json.loads((book_dir / 'meta.json').read_bytes())
+cover_id = meta["opf_unique_identifier_type"][1:]
 
-cover_image = book_dir / f'files/images/{meta["opf_unique_identifier_type"][1:]}.jpg'
+cover_image = book_dir / f'files/images/{cover_id}.jpg'
 if not cover_image.exists():
   print(f'Cover images does not exist: {cover_image}')
   sys.exit(1)
 
 title = meta['title']
-authors = [item['name'] for item in meta['authors']]
+authors = ' & '.join(item['name'] for item in meta['authors'])
+output_file = f'{title} - {authors}.epub'
 
 cmd = [
   'ebook-convert',
   str(book_dir / 'nav.html'),
-  f'{title} - {authors[0]}.epub',
+  output_file,
   '--title', title,
-  '--authors', ' & '.join(authors),
+  '--authors', authors,
   '--publisher', meta['publishers'][0]['name'],
-  '--comments', html2text(meta['description']),
+  '--comments', meta['description'],
   '--cover', str(cover_image),
 ]
 subprocess.call(cmd)
