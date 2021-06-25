02397e2f1adcc24e189b4cd3a00561c07514b577
Feihong Hsu <feihong.hsu@gmail.com>
2021-06-25 13:13:46 -0500

    Extract toc file as well

--- tech/extract_files.py
+++ tech/extract_files.py
@@ -18,7 +18,7 @@ html_suffix = """
 </body>
 </html>"""
 
-# Extract metadata file
+# Extract metadata and toc files
 cur.execute("select path, data from dump where path like '/api/v1/book/%/'")
 meta = None
 for row in cur.fetchall():
@@ -34,6 +34,12 @@ path.write_text(json.dumps(meta, indent=2))
 
 book_id = meta['identifier']
 print(f'Title: {meta["title"]}, Id: {book_id}')
+
+cur.execute(f"select path, data from dump where path = '/api/v1/book/{book_id}/toc/'")
+_, data = cur.fetchone()
+toc = json.loads(data)
+path = contents_dir / 'toc.json'
+path.write_text(json.dumps(toc, indent=2))
 
 # Extract textual content
 cur.execute(f"""
