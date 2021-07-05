aec3ecebcd50333d08cf831d2d92f20cba837af6
Feihong Hsu <feihong.hsu@gmail.com>
2021-07-05 14:19:00 -0500

    Refactor generate_cbz_files to generate from folders instead of database

--- manhua-2/generate_cbz_files.py
+++ manhua-2/generate_cbz_files.py
@@ -1,43 +1,59 @@
 import json
-import sqlite3
+from pathlib import Path
+import subprocess
 from zipfile import ZipFile, ZIP_STORED
 
-filename = 'dumpfile.db'
-conn = sqlite3.connect(filename)
-cur = conn.cursor()
+input_root = Path('output')
 
-cur.execute("""
-select data from dump
-where path like '%getSectionDownloadInfo%'""")
+def split_image(image, image_file):
+  left_file = image_file.with_name(image_file.stem + '_l' + image_file.suffix)
+  right_file = image_file.with_name(image_file.stem + '_r' + image_file.suffix)
+  w = image['width']
+  h = image['height']
 
-sections = {}
+  subprocess.call([
+    'magick', 'convert', str(image_file), '-crop',
+    f'{w / 2}x{h}+0+0',
+    str(left_file),
+  ])
+  subprocess.call([
+    'magick', 'convert', str(image_file), '-crop',
+    f'{w / 2}x{h}+{w / 2}+0',
+    str(right_file),
+  ])
+  return left_file, right_file
 
-for row in cur.fetchall():
-  obj = json.loads(row[0])
-  section = obj['response']['sectionItem']
-  sections[section['mangaSectionId']] = section
 
-  print(f'{section["mangaName"]} {section["mangaSectionName"]} {section["mangaSectionTitle"]}')
+for input_dir in input_root.iterdir():
+  if not input_dir.is_dir():
+    continue
 
-sections = sections.values()
+  output_file = input_dir.name + '.cbz'
 
-for section in sections:
-  is_complete = True
+  meta = json.loads(Path(input_dir / 'meta.json').read_bytes())
 
-  output_file = f'{section["mangaName"]} {section["mangaSectionName"]} {section["mangaSectionTitle"]}'.strip() + '.cbz'
+  def get_images():
+    for (name, item) in zip(meta['mangaSectionImages'], meta['inciseInfo']):
+      item['name'] = name[1:]
+      yield item
 
-  images = []   # list of bytestrings
-  for image_path in section['mangaSectionImages']:
-    cur.execute(f"""
-    select data from dump
-    where path like '%{image_path}%';
-    """)
-    images.append(cur.fetchone()[0])
+  images = list(get_images())
 
   with ZipFile(output_file, 'w') as zf:
-    for i, data in enumerate(images, 1):
-      zf.writestr(f'{i:03}.jpg', data, compress_type=ZIP_STORED)
+    count = 1
+    for image in images:
+      image_file = input_dir / image['name']
+      print(image_file)
+
+      if image['pageType'] == 0:
+        # ZIP_STORED means no compression
+        zf.writestr(f'{count:03}.jpg', image_file.read_bytes(), compress_type=ZIP_STORED)
+        count += 1
+      elif image['pageType'] == 1:
+        # Cut into 2 pages
+        left_file, right_file = split_image(image, image_file)
+        zf.writestr(f'{count:03}.jpg', right_file.read_bytes(), compress_type=ZIP_STORED)
+        zf.writestr(f'{count + 1:03}.jpg', left_file.read_bytes(), compress_type=ZIP_STORED)
+        count += 2
 
   print(f'Wrote {len(images)} images to {output_file}')
-
-conn.close()
