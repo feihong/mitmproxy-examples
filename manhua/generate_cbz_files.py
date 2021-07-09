80c726ec49c416ac9cacbb3845141e3a967aadd6
Feihong Hsu <feihong.hsu@gmail.com>
2021-07-08 21:43:27 -0500

    Make generate_cbz_files.py more maintainable

--- manhua/generate_cbz_files.py
+++ manhua/generate_cbz_files.py
@@ -7,38 +7,40 @@ filename = 'dumpfile.db'
 conn = sqlite3.connect(filename)
 cur = conn.cursor()
 
-cur.execute("""
-select data from dump
-where path like '%GetImageIndex';
-""")
+def get_comics():
+  comics = {}
+  cur.execute("select data from dump where path like '%ComicDetail%';")
+  for row in cur.fetchall():
+    comic = json.loads(row[0])['data']
+    comic_id = str(comic['id'])
+    comics[comic_id] = comic
 
-comics = {}
-episodes = {}
+  return comics
 
-for row in cur.fetchall():
-  obj = json.loads(row[0])
-  episode = obj['data']
-  path = episode['path']
-  if not path: continue
-  match = re.match(r"/bfs/manga/(\d+)/(\d+)", episode['path'])
-  comic_id = match.group(1)
-  episode['comic_id'] = comic_id
-  episode_id = int(match.group(2))
-  episode['id'] = episode_id
-  episodes[episode_id] = episode
-  # print(f'{comid_id} {episode_id}')
+comics = get_comics()
 
-episodes = episodes.values()
+def get_episodes():
+  episodes = {}
+  cur.execute("select data from dump where path like '%GetImageIndex%';")
+
+  for row in cur.fetchall():
+    episode = json.loads(row[0])['data']
+    path = episode['path']
+    if not path: continue
+    match = re.match(r"/bfs/manga/(\d+)/(\d+)", episode['path'])
+    comic_id = match.group(1)
+    episode['comic'] = comics[comic_id]
+    episode_id = int(match.group(2))
+    episode['id'] = episode_id
+    episodes[episode_id] = episode
+    print(f'{comic_id} {episode_id}')
+
+  return episodes.values()
+
+episodes = get_episodes()
 
 for episode in episodes:
-  comic = comics.get(comic_id)
-  if comic is None:
-    cur.execute(f"""
-    select data from dump
-    where path like '%ComicDetail' and request_params like '%"comic_id": "{comic_id}"%';
-    """)
-    comic = json.loads(cur.fetchone()[0])['data']
-    comic[comic_id] = comic
+  comic = episode['comic']
 
   try:
     detail = [ep for ep in comic['ep_list'] if ep['id'] == episode['id']][0]
@@ -65,7 +67,8 @@ for episode in episodes:
   warning = ''
   if len(images) < len(episode['images']):
     warning = f", should be {len(episode['images'])}"
-  output_file = f"{comic['title']} {detail['short_title']} {detail['title']}({len(images)}{warning}).cbz"
+  output_file = f"{comic['title']} {detail['short_title']} {detail['title']} ({len(images)}{warning}).cbz"
+
   with ZipFile(output_file, 'w') as zf:
     for i, data in enumerate(images, 1):
       zf.writestr(f'{i:03}.jpg', data, compress_type=ZIP_STORED)
